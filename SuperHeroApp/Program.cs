c6fde8c99b18a50dfb1356a13fcb87bb02669661
Greta Kaminskaitė <greta.kam@gmail.com>
2022-02-24 20:48:25 +0200

    Added Super Hero variables and data types

--- SuperHeroApp/Program.cs
+++ SuperHeroApp/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+string HeroName = "Smarty-pants";
+int HeroAge = 22;
+string HeroPower1, HeroPower2, HeroPower3;
+HeroPower1 = "hyperthymesia";
+HeroPower2 = "learns everything very fast";
+HeroPower3 = "can do any mathematical calculation in their head in 0,1s";
+
+Console.WriteLine("><><><><><><><><><><><><><><><><><><><><><><><><><\n");
+Console.WriteLine($"Hero name: {HeroName}");
+Console.WriteLine($"Hero age: {HeroAge}");
+Console.WriteLine($"Hero powers: {HeroPower1}, {HeroPower2}, {HeroPower3}");
+Console.WriteLine("\n><><><><><><><><><><><><><><><><><><><><><><><><><\n");
+
+double number = (-5 +8*6)*6;
+Console.WriteLine(number);
+
