0917bb43488074ae704f0748ef256c7169785f67
Greta Kaminskaite <greta.kam@gmail.com>
2022-03-17 21:49:25 +0200

    added a menu

---
+++ SuperHeroApp/ArrayHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroApp
+{
+    public static class ArrayHelper
+    {
+
+        public static T[] Append<T>(this T[] array, T item)
+        {
+            List<T> list = new List<T>(array);
+            list.Add(item);
+
+            return list.ToArray();
+        }
+
+
+        public static T[] RemoveAtPosition<T>(this T[] array, int position)
+        {
+            List<T> list = new List<T>(array);
+            list.RemoveAt(position);
+
+            return list.ToArray();
+        }
+
+        public static string[] RemoveByName( string[] array, string position) 
+        { List<string> list = new List<string>(array); 
+            list.Remove(position); 
+            return list.ToArray(); 
+        }
+
+    }
+}
--- SuperHeroApp/Program.cs
+++ SuperHeroApp/Program.cs
@@ -1,6 +1,113 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+bool menuRunning = true;
+string[] superHeroArray = { "Smarty-pants", "Flash", "Captain Marvel", "Black widow" };
+do
+{
+    Console.WriteLine("Welcome to the SuperHero application!");
+    Console.WriteLine("Please select from the menu what you would like to do:");
+    Console.WriteLine("1 - Show a list of superheroes");
+    Console.WriteLine("2 - Show specific hero");
+    Console.WriteLine("3 - Add a new a superhero");
+    Console.WriteLine("4 - Delete a superhero");
+    Console.WriteLine("5 - Exit");
+
+    string menuAction = Console.ReadLine();
+
+    switch (menuAction)
+    {
+        case "1":
+            Console.WriteLine("==============SUPERHERO=LIST==============");
+            for (int i = 0; i < superHeroArray.Length; i++)
+            {
+                Console.WriteLine($"{i}. {superHeroArray[i]}");
+            }
+            Console.WriteLine("===========================================");
+            break;
+        case "2":
+            Console.WriteLine("==============SUPERHERO=INFORMATION==============");
+            Console.WriteLine("Please write the ID number of the superhero that you want to know more about");
+            Console.WriteLine("List of heroes:");
+            for (int i = 0; i < superHeroArray.Length; i++)
+            {
+                Console.WriteLine($"{i}. {superHeroArray[i]}");
+            }
+            
+            
+            int.TryParse(Console.ReadLine(), out int superHeroID);
+            Console.WriteLine($"Please select from the menu what you want to know about {superHeroArray[superHeroID]}");
+            Console.WriteLine("1 - General info");
+            Console.WriteLine("2 - Financial info");
+            int.TryParse(Console.ReadLine(), out int menuItem);
+
+            switch (menuItem)
+            {
+                case 1:
+                    Console.WriteLine("=====================GENERAL=INFO=====================\n");
+                    Console.WriteLine($"Hero name: {superHeroArray[superHeroID]}");
+                    Console.WriteLine($"Hero age: 22");
+                    Console.WriteLine($"Hero powers: HeroPower1, HeroPower2, HeroPower3");
+                    Console.WriteLine("\n====================================================\n");
+                    break;
+                case 2:
+                    Console.WriteLine("===================FINANCIAL=INFO===================\n");
+                    double dailySalary2 = 80;
+                    Console.WriteLine($"The hero's daily salary is {dailySalary2}");
+                    Console.WriteLine($"The hero's monthly salary is {dailySalary2 * 30}");
+                    double cookieCost2 = 1.29;
+                    double boughtCookies2 = Math.Floor(dailySalary2 * 30 / cookieCost2);
+                    Console.WriteLine($"The hero can buy {boughtCookies2} cookies from his salary");
+                    Console.WriteLine("===========================================\n");
+
+                    break;
+                default:
+                    Console.WriteLine("Incorrect number");
+                    break;
+            }
+            break;
+
+        case "3":
+            Console.WriteLine("===============NEW=SUPERHERO==================");
+            Console.WriteLine("Please enter the name of the new superhero!");
+            string newSuperHero = Console.ReadLine();
+
+            List<string> list2 = new List<string>(superHeroArray);
+            list2.Add(newSuperHero);             
+            superHeroArray = list2.ToArray();             
+
+            break;
+        case "4":
+            Console.WriteLine("===============DELETE=SUPERHERO==================");
+            
+            for (int i = 0; i < superHeroArray.Length; i++)
+            {
+                Console.WriteLine($"{i}. {superHeroArray[i]}");
+            }
+            Console.WriteLine("\n Please enter the number of the hero that you want to delete");
+            int deleteHeroNumber = Convert.ToInt32(Console.ReadLine());
+
+            List <string> list = new List<string>(superHeroArray);
+            list.RemoveAt(deleteHeroNumber);            
+            superHeroArray = list.ToArray();
+
+            Console.WriteLine("You have successfully deleted the hero");
+            Console.WriteLine("===========================================");
+
+            break;
+        case "5":
+            Console.WriteLine("Thank you for using SuperHero application! Goodbye!");
+            menuRunning = false;
+            break;
+        default:
+            Console.WriteLine("Please enter a correct number");
+            Console.WriteLine("===========================================");
+            break;
+    }
+
+}
+while (menuRunning == true);
 
 /*
+//GENERAL INFO
 string HeroName = "Smarty-pants";
 int HeroAge = 22;
 string HeroPower1, HeroPower2, HeroPower3;
@@ -8,16 +115,15 @@ HeroPower1 = "hyperthymesia";
 HeroPower2 = "learns everything very fast";
 HeroPower3 = "can do any mathematical calculation in their head in 0,1s";
 
-Console.WriteLine("><><><><><><><><><><><><>GENERAL INFO<><><><><><><><><><><><><\n");
+Console.WriteLine("=====================GENERAL=INFO=====================\n");
 Console.WriteLine($"Hero name: {HeroName}");
 Console.WriteLine($"Hero age: {HeroAge}");
 Console.WriteLine($"Hero powers: {HeroPower1}, {HeroPower2}, {HeroPower3}");
-Console.WriteLine("\n><><><><><><><><><><><><><><><><><><><><><><><><><\n\n\n");
-*/
+Console.WriteLine("\n====================================================\n\n");
 
 
-
-Console.WriteLine("><><><><><><><><><><><><>FINANCIAL INFO<><><><><><><><><><><><><\n");
+//FINANCIAL INFO
+Console.WriteLine("===================FINANCIAL=INFO===================\n");
 
 int workingHoursInDay;
 Console.Write("Please enter hero working hours: ");
@@ -79,5 +185,6 @@ else
     Console.WriteLine("The hero is actually a vilain and steels from cookie supply");
 }
 
-Console.WriteLine("\n><><><><><><><><><><><><><><><><><><><><><><><><><\n");
+Console.WriteLine("\n====================================================\n");
 
+*/
